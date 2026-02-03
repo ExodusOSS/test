41954daf7a6ae4008626f018ccbd44174caefc75
Nikita Skovoroda <chalkerx@gmail.com>
2026-02-03 13:55:18 +0400

    doc: add index export with a usage notice

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * ### The `@exodus/test` package consists of submodules, there is no single export.
+ * Import specific submodules instead.
+ *
+ * Straightforward setups:
+ * 1. Import `node:test` and run with `@exodus/test`
+ * 2. Use jest globals and run with `@exodus/test --jest`
+ * 3. Import `@exodus/test/tape` instead of tape or tap and run with `@exodus/test` (or `node`)
+ *
+ * See [README](https://github.com/ExodusOSS/test/blob/main/README.md).
+ */
+declare module '@exodus/test' {}
